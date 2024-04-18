f1bb360ee8faf46fd1c1a574d4d32db13dbf4c5e
brian <brianxue318@gmail.com>
2024-04-18 09:12:05 +0800

    add 3 expressions

--- test/lang_parser/test_class_decl.py
+++ test/lang_parser/test_class_decl.py
@@ -2,7 +2,8 @@
 
 import os
 
-command = os.path.realpath("/app/experiment_2/src/lian/lang/main.py") + " --lang=java -debug -print_statements " + os.path.realpath("/app/experiment_2/test/cases/class_decl.java")
+# command = os.path.realpath("/app/experiment_2/src/lian/lang/main.py") + " --lang=java -debug -print_statements " + os.path.realpath("/app/experiment_2/test/cases/class_decl.java")
+command = os.path.realpath("/app/experiment_2/src/lian/lang/main.py") + " --lang=typescript -debug -print_statements " + os.path.realpath("/app/experiment_2/test/cases/expr.ts")
 
 print(command)
 
