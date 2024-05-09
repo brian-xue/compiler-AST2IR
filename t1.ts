33b23afb8639059265d6d608665039dc097e00cf
Fork512Hz <162413375+Fork512Hz@users.noreply.github.com>
2024-05-09 21:41:19 +0800

    Add files via upload
    Expression_statement有些问题，虽然这边不是实验文档里需要的，但是是For和某些其他语句的Prerequisite。现在这个版本会出现Expression_statement复读其他expression的情况（IR里同时有两者），我不太清楚怎么处理

---
+++ t1.ts
@@ -0,0 +1,21 @@
+while(x > 0) 
+    x = x / 2;
+
+do
+    x = x / 2;
+while(x > 0);
+
+for(con; s>5; a++) 
+    foo = 1;
+
+if(cond)
+    doThis + 4;
+else
+    doThat - 3;
+
+switch(a)
+{
+    case 1: abCS;
+    case 5: cd+Ke;
+    default: efbs;
+}
